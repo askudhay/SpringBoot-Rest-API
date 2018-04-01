eb9897ae0f229cf7cc48fc93483b3677a14d42ad
Udhay <systemudhaya@gmail.com>
2018-03-31 22:10:52 -0400

    Example 2

---
+++ SpringBootHelloWorld/src/main/java/com/springboot/ex/Application.java
@@ -0,0 +1,17 @@
+package com.springboot.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Main class
+ * 
+ * @author Udhay
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
